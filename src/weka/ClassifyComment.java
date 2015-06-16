029f4cabb3b55f25f59bd39229e2d4e7059a2a48
FernandoJHO <fernando.handal@yahoo.com>
2015-06-15 22:50:37 -0300

    codigo weka

--- src/weka/ClassifyComment.java
+++ src/weka/ClassifyComment.java
@@ -7,15 +7,16 @@ public class ClassifyComment {
     
     public ClassifyComment(){};
 
-    public String classify(String comment) throws Exception {
-
+    public String classify(String comment,String ruta_modelo,String ruta_attr) throws Exception {
+        //C:/Users/Fernando/Desktop/weka/svm.model
+        //C:/Users/Fernando/Desktop/weka/attrs.dat
         TrainModelForSerialize tmfs = new TrainModelForSerialize();
         CommentClassifier cs;
         Tokenizer tokenizer = new SimpleTokenizer(new Locale("ES"));
         StopWordsRemoval stopWordsRemoval = new StopWordsRemoval();
         double classif;
 
-        cs = new CommentClassifier("C:/Users/Fernando/Desktop/weka/svm.model", "C:/Users/Fernando/Desktop/weka/attrs.dat", tokenizer, stopWordsRemoval);
+        cs = new CommentClassifier(ruta_modelo, ruta_attr, tokenizer, stopWordsRemoval);
         classif = cs.classify(comment);
 
         if (classif == 0.0) {
